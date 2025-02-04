e501f346f0edd56713cf7f91d6e9285e4ccd3de4
Ruoqi Huang <huangruoqi374@gmail.com>
2025-02-03 23:06:44 -0800

    nice progress

--- src/parser.ts
+++ src/parser.ts
@@ -15,12 +15,7 @@ export const getProbSaveLocation = (srcPath: string): string => {
     const savePreference = getSaveLocationPref();
     const srcFileName = path.basename(srcPath);
     const srcFolder = path.dirname(srcPath);
-    const hash = crypto
-        .createHash('md5')
-        .update(srcPath)
-        .digest('hex')
-        .substr(0);
-    const baseProbName = `.${srcFileName}_${hash}.prob`;
+    const baseProbName = `.${srcFileName}.prob`;
     const cphFolder = path.join(srcFolder, '.cph');
     if (savePreference && savePreference !== '') {
         return path.join(savePreference, baseProbName);
